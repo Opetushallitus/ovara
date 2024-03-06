aae665fbfa0a90883e29f5da55845ec18e46c29d
August Kilponen <august.kilponen@gmail.com>
2024-03-06 13:35:39 +0200

    Siivottu koodia

--- cdk/lib/s3-stack.ts
+++ cdk/lib/s3-stack.ts
@@ -22,15 +22,6 @@ export class S3Stack extends cdk.Stack {
       encryptionKey: new kms.Key(this, `${siirtotiedostotBucketName}-s3BucketKMSKey`),
     });
 
-    //const opintopolkuAccountPrincipal = new iam.AccountPrincipal(config.opintopolkuAccountId);
-    /*
-    siirtotiedostotS3Bucket.addToResourcePolicy(new iam.PolicyStatement({
-      actions: ['s3:*'],
-      resources: [siirtotiedostotS3Bucket.arnForObjects('*')],
-      principals: [opintopolkuAccountPrincipal]
-    }));
-     */
-
     const s3CrossAccountRole = new iam.Role(this, 'OpintopolkuS3CrossAcountRole', {
       assumedBy: new iam.ArnPrincipal(`arn:aws:iam::${config.opintopolkuAccountId}:root`),
       roleName: 'opintopolku-s3-cross-account-role',
