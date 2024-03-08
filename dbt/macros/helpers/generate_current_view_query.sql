c8b596f47dd9c9706ee14d35374da681eedfac9a
Matz Rasmus <matz.rasmus@knowit.fi>
2024-03-08 15:55:52 +0200

    Päivitetty makrot toimimaan postgresissa

--- dbt/macros/helpers/generate_current_view_query.sql
+++ dbt/macros/helpers/generate_current_view_query.sql
@@ -5,10 +5,10 @@
 {% macro generate_current_view_query(model_name) %}
 
 {%- set dw_relation_name = model_name -%}
-{%- set key_column_name = 'dw_metadata_' + model_name + '_key' -%}
-{%- set hash_column_name = 'dw_metadata_' + model_name + '_hash' -%}
-{%- set data_timestamp_name = 'dw_metadata_' + model_name + '_timestamp' -%}
-{%- set dw_stored_at_name = 'dw_metadata_' + model_name + '_dw_stored_at' -%}
+{%- set key_column_name = 'dw_metadata_key' -%}
+{%- set hash_column_name = 'dw_metadata_hash' -%}
+{%- set data_timestamp_name = 'dw_metadata_timestamp' -%}
+{%- set dw_stored_at_name = 'dw_metadata_dw_stored_at' -%}
 
 {%- set query_string %}
 with _latest_hash_per_key as (
@@ -23,14 +23,25 @@ with _latest_hash_per_key as (
             {{ dw_stored_at_name }} desc
         ) as _row_order
     from {{ ref(dw_relation_name) }}
-    qualify _row_order = 1
 )
 
-select * exclude _row_order
-from _latest_hash_per_key
+select 
 
-{% endset %}
+{%- set column_names = dbt_utils.get_filtered_columns_in_relation(from=this, except=['_row_order']) %}
+{% for column_name in column_names -%}
+   {{ column_name }}
+   {%- if not loop.last %}
+    ,
+    {%- endif -%}
+{% endfor -%}
+
+
+
+from _latest_hash_per_key
+where _row_order = 1
+
+{%- endset %}
 
 {{ return(query_string) }}
 
-{% endmacro %}
+{% endmacro -%}
