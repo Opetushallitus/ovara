3b38671e3bee3c44317406c87df499515e3a8f61
Matz Rasmus <matz.rasmus@knowit.fi>
2024-04-15 09:03:28 +0300

    LIsätty valintaperuste

---
+++ dbt/models/dw/dw_kouta_valintaperuste.sql
@@ -0,0 +1,14 @@
+{{
+  config(
+    indexes = [{'columns':['id','muokattu']}]
+    )
+}}
+
+{%- set stage_model = ref('stg_kouta_valintaperuste') -%}
+{%- set key_columns_list = ['id'] -%}
+
+with current_rows as (
+    {{ generate_dw_model(stage_model, key_columns_list) }}
+)
+
+select * from current_rows
