d2cf70d80b7ceeaf1c186aa4bd7431276524e480
Matz Rasmus <matz.rasmus@knowit.fi>
2024-04-17 14:05:49 +0300

    Lisätty hakemukselle dw-malli

---
+++ dbt/models/dw/dw_ataru_hakemus.sql
@@ -0,0 +1,15 @@
+{{
+  config(
+    indexes = [{"columns":['oid','versio_id','muokattu']}]
+    )
+}}
+
+
+{%- set stage_model = ref('stg_ataru_hakemus') -%}
+{%- set key_columns_list = ['oid','versio_id'] -%}
+
+with current_rows as (
+    {{ generate_dw_model(stage_model, key_columns_list) }}
+)
+
+select * from current_rows
