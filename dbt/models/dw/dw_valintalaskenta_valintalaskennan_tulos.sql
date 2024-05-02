8e8015cf55ccefee04db55e76323862663fbbb48
Matz Rasmus <matz.rasmus@knowit.fi>
2024-05-02 13:30:01 +0300

    LIsätty valintalaskennan tulos-taulut

---
+++ dbt/models/dw/dw_valintalaskenta_valintalaskennan_tulos.sql
@@ -0,0 +1,14 @@
+{{
+  config(
+    indexes = [{'columns':['valinnanvaihe_id','muokattu']}]
+      )
+}}
+
+{%- set stage_model = ref('stg_valintalaskenta_valintalaskennan_tulos') -%}
+{%- set key_columns_list = ['valinnanvaihe_id','muokattu'] -%}
+
+with current_rows as (
+    {{ generate_dw_model(stage_model, key_columns_list) }}
+)
+
+select * from current_rows
