d12506add20da14ccc7f3f2747d7b90af3833a52
Matz Rasmus <matz.rasmus@knowit.fi>
2024-05-31 15:00:01 +0300

    sqlfluff'd mallit

--- dbt/models/stg/stg_hakukohderyhmapalvelu_ryhma.sql
+++ dbt/models/stg/stg_hakukohderyhmapalvelu_ryhma.sql
@@ -1,19 +1,19 @@
 with source as (
-      select * from {{ source('ovara', 'hakukohderyhmapalvelu_ryhma') }}
+    select * from {{ source('ovara', 'hakukohderyhmapalvelu_ryhma') }}
 
-      {% if is_incremental() %}
+    {% if is_incremental() %}
 
-       where dw_metadata_dbt_copied_at > (select max(dw_metadata_dbt_copied_at) from {{ this }})
+        where dw_metadata_dbt_copied_at > (select max(dw_metadata_dbt_copied_at) from {{ this }})
 
     {% endif %}
 ),
 
-final as
-(
+final as (
     select
         data ->> 'hakukohderyhma-oid' as oid,
         (data -> 'hakukohde-oids')::jsonb as hakukohde_oid,
-        (data -> 'settings' ->> 'jos-ylioppilastutkinto-ei-muita-pohjakoulutusliitepyyntoja')::boolean jos_ylioppilastutkinto_ei_muita_pohjakoulutusliitepyyntoja,
+        (data -> 'settings' ->> 'jos-ylioppilastutkinto-ei-muita-pohjakoulutusliitepyyntoja')::boolean
+        as jos_ylioppilastutkinto_ei_muita_pohjakoulutusliitepyyntoja,
         (data -> 'settings' ->> 'max-hakukohteet')::int as max_hakukohteet,
         (data -> 'settings' ->> 'priorisoiva')::boolean as priorisoiva,
         (data -> 'settings' -> 'prioriteettijarjestys')::jsonb as prioriteettijarjestys,
@@ -21,7 +21,6 @@ final as
         (data -> 'settings' ->> 'yo-amm-autom-hakukelpoisuus')::boolean as yo_amm_autom_hakukelpoisuus,
         {{ metadata_columns() }}
     from source
-
 )
 
 select * from final
