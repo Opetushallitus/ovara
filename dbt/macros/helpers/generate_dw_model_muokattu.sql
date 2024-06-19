3ccf65a976a852f568c8d99b258cc138e2d91ca2
Matz Rasmus <matz.rasmus@knowit.fi>
2024-06-19 10:47:52 +0300

    improvements to DW macro
    (cherry picked from commit d523e73d2e818ece0384b053c149b0b68e39a82f)

---
+++ dbt/macros/helpers/generate_dw_model_muokattu.sql
@@ -0,0 +1,62 @@
+{% macro generate_dw_model_muokattu(src_model,key_columns_list) %}
+{#
+    src_model needs to be provided as ref(), key_columns_list as an array
+#}
+
+{{-
+    config(
+        materialized = 'incremental',
+        incremental_strategy = 'merge',
+        full_refresh = false,
+        on_schema_change = 'append_new_columns',
+        unique_key = key_columns_list,
+        merge_exclude_columns = [
+            'dw_metadata_dbt_copied_at',
+            'dw_metadata_file_row_number',
+            'dw_metadata_filename',
+            'dw_metadata_source_timestamp_at',
+            'dw_metadata_stg_stored_at',
+            'dw_metadata_dw_stored_at'
+            ],
+        indexes = [
+            {
+                'columns': key_columns_list,
+                'columns': ['dw_metadata_dw_stored_at']
+            }
+        ]
+    )
+-}}
+
+with _raw as (
+    select
+    *,
+    row_number() over (partition by
+    {% for column in key_columns_list -%}
+        {{column}}
+        {%- if not loop.last -%}
+        ,
+        {%- endif -%}
+    {%- endfor %}
+    order by dw_metadata_dbt_copied_at desc) as rownr
+    from
+    {{ src_model }}
+    {% if is_incremental() -%}
+    {# Only rows which are newer than the rows in dw model table already #}
+    where (dw_metadata_stg_stored_at >= coalesce((select max(dw_metadata_stg_stored_at) from {{ this }}),date('1900-01-01'))
+            or dw_metadata_stg_stored_at is null)
+    {%- endif %}
+
+),
+date as (
+    select *,
+    current_timestamp as dw_metadata_dw_stored_at
+    from _raw where rownr=1
+),
+
+final as (
+    select
+        {{ dbt_utils.star(from=src_model) }},
+        dw_metadata_dw_stored_at
+    from date
+)
+{% endmacro %}
