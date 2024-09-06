ed8cfb6f18b8fff4f34ce482cd744d6d802e0df0
Matz Rasmus <matz.rasmus@knowit.fi>
2024-09-06 13:16:38 +0300

    LIsätty lataaminen asiasana ja ammattinimike

---
+++ dbt/models/stg/stg_kouta_asiasana.sql
@@ -0,0 +1,19 @@
+with source as (
+    select * from {{ source('ovara', 'kouta_asiasana') }}
+
+    {% if is_incremental() %}
+
+        where dw_metadata_dbt_copied_at > (select max(dw_metadata_dbt_copied_at) from {{ this }})
+
+    {% endif %}
+),
+
+final as (
+    select
+        data ->> 'kieli' as kieli,
+        data ->> 'arvo' as arvo,
+        {{ metadata_columns() }}
+    from source
+)
+
+select * from final
