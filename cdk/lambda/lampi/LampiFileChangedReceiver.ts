aa9c8ba2f67f9ef52ae77dd08a9f2f987dd5a078
August Kilponen <august.kilponen@gmail.com>
2024-10-02 15:44:45 +0300

    Laitettu Lampi-lambdan autentikaatio päälle

--- cdk/lambda/lampi/LampiFileChangedReceiver.ts
+++ cdk/lambda/lampi/LampiFileChangedReceiver.ts
@@ -1,20 +1,51 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
 import { SendMessageCommand, SQSClient } from '@aws-sdk/client-sqs';
 import { APIGatewayProxyEventV2 } from 'aws-lambda';
 import { Context } from 'aws-lambda/handler';
 
 import { LampiEvent, LampiS3Event, lampiKeyExists } from './common';
 
+const { SSMClient, GetParameterCommand } = require('@aws-sdk/client-ssm');
+
 exports.handler = async (event: APIGatewayProxyEventV2, context: Context) => {
+  const awsRegion = process.env.AWS_REGION;
+  console.log(`AWS Region: ${awsRegion}`);
+
+  const lampiAuthTokenSecretName = process.env.lampiAuthTokenSecretName;
+  console.log(`lampiAuthTokenSecretName: ${lampiAuthTokenSecretName}`);
+
+  const parameterCommand = new GetParameterCommand({
+    Name: lampiAuthTokenSecretName,
+    WithDecryption: true,
+  });
+
+  const ssmClient = new SSMClient({ region: awsRegion });
+  const ssmResponse = await ssmClient.send(parameterCommand);
+
+  const lampiAuthToken = ssmResponse.Parameter.Value;
+  console.log(`lampiAuthToken: ${lampiAuthToken}`);
+
   console.log(JSON.stringify(event, null, 4));
+
   if (!event?.body) {
     console.error('Viestissä ei ollut bodya tai viesti oli tyhjä');
     return {
       statusCode: 500,
     };
   }
+
   const lampiEvent: LampiEvent = JSON.parse(event.body);
+
+  if (lampiEvent.token !== lampiAuthToken) {
+    console.error('Autentikaatio epäonnistui!');
+    return {
+      statusCode: 401,
+    };
+  }
+
   const lampiS3Event: LampiS3Event = lampiEvent.s3;
   const lampiKey = lampiS3Event.object.key;
+
   if (lampiKeyExists(lampiKey)) {
     console.log(
       `Uusi tunnistettu tiedosto saapunut Lampeen (${lampiKey}). Lähetetään tiedosto ladattavaksi.`
@@ -31,6 +62,7 @@ exports.handler = async (event: APIGatewayProxyEventV2, context: Context) => {
   } else {
     console.log(`Tuntematon tiedosto: ${lampiKey}`);
   }
+
   return {
     statusCode: 200,
   };
