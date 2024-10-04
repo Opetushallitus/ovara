2339d9effd2ef0a6b65bfbe477ede999bc7231e2
August Kilponen <august.kilponen@gmail.com>
2024-10-04 12:00:22 +0300

    Docker build ja push AWS:n ECR:ään

--- utility/cdk/lib/ecr-stack.ts
+++ utility/cdk/lib/ecr-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as cdkNag from 'cdk-nag';
 import { Construct } from 'constructs';
 
 export class EcrStack extends cdk.Stack {
@@ -11,5 +13,31 @@ export class EcrStack extends cdk.Stack {
       repositoryName: dbtRunnerRepositoryName,
       imageScanOnPush: true,
     });
+
+    const githubOidcProvider = new iam.OpenIdConnectProvider(this, `OvaraUtilityGithubOidcProvider`, {
+      url: 'https://token.actions.githubusercontent.com',
+      thumbprints: ['6938fd4d98bab03faadb97b34396831e3780aea1'],
+      clientIds: ['sts.amazonaws.com'],
+    });
+
+    const githubActionsDeploymentRole = new iam.Role(this, `OvaraUtilityGithubActionsUser`, {
+      assumedBy: new iam.WebIdentityPrincipal(
+        githubOidcProvider.openIdConnectProviderArn,
+        {
+          StringLike: {
+            'token.actions.githubusercontent.com:sub': 'repo:opetushallitus/ovara',
+            'token.actions.githubusercontent.com:aud': 'sts.amazonaws.com',
+          },
+        },
+      ),
+      roleName: 'ovara-utility-github-actions-deployment-role',
+    });
+
+    dbtRunnerRepository.grantPush(githubActionsDeploymentRole);
+
+    cdkNag.NagSuppressions.addStackSuppressions(this, [
+      { id: 'AwsSolutions-IAM5', reason: 'In this case it is ok.' },
+    ]);
+
   }
 }
