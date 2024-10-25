2cc6512158aa21c2f71756ac17fe415f3f8476cf
Matz Rasmus <matz.rasmus@knowit.fi>
2024-10-25 16:32:09 +0300

    Lisätty lokitusta siivoukselle

--- dbt/macros/helpers/tempdata_cleanup.sql
+++ dbt/macros/helpers/tempdata_cleanup.sql
@@ -5,9 +5,9 @@
     {% set tables_list = tables.rows %}
     {% for row in tables_list %}
         {% set table = row[0] %}
+        {{ print ('Cleaning table '+table) }}
         {% set sql = 'delete from raw.'+table+' where dw_metadata_dbt_copied_at < (select start_time from raw.completed_dbt_runs where raw_table = \''+table+'\')' %}
         {% do run_query(sql) %}
-        --{{ print (sql) }}
         {% set sql = 'delete from stg.stg_'+table+' where dw_metadata_dbt_copied_at < (select start_time from raw.completed_dbt_runs where raw_table = \''+table+'\')' %}
         {% do run_query(sql) %}
     {% endfor %}
