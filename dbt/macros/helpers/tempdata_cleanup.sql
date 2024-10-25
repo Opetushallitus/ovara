d9694f6c0e2c35b28272f72b68f32025b58d7648
Matz Rasmus <matz.rasmus@knowit.fi>
2024-10-25 16:26:32 +0300

    Korjattu siivousmakro

--- dbt/macros/helpers/tempdata_cleanup.sql
+++ dbt/macros/helpers/tempdata_cleanup.sql
@@ -5,13 +5,11 @@
     {% set tables_list = tables.rows %}
     {% for row in tables_list %}
         {% set table = row[0] %}
-        {% if table == 'kouta_haku' %}
-            {% set sql = 'delete from raw.'+table+' where dw_metadata_dbt_copied_at < (select start_time from raw.completed_dbt_runs where raw_table = \''+table+'\')' %}
-            {% do run_query(sql) %}
-            --{{ print (sql) }}
-            {% set sql = 'delete from stg.stg_'+table+' where dw_metadata_dbt_copied_at < (select start_time from raw.completed_dbt_runs where raw_table = \''+table+'\')' %}
-            {% do run_query(sql) %}
-        {% endif %}
+        {% set sql = 'delete from raw.'+table+' where dw_metadata_dbt_copied_at < (select start_time from raw.completed_dbt_runs where raw_table = \''+table+'\')' %}
+        {% do run_query(sql) %}
+        --{{ print (sql) }}
+        {% set sql = 'delete from stg.stg_'+table+' where dw_metadata_dbt_copied_at < (select start_time from raw.completed_dbt_runs where raw_table = \''+table+'\')' %}
+        {% do run_query(sql) %}
     {% endfor %}
 {% endif %}
 {%- endmacro %}
