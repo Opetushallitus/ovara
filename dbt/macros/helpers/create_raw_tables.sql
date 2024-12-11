bc58dc765320c3a4c75e35205c5503bf64c16885
Matz Rasmus <matz.rasmus@knowit.fi>
2024-12-11 09:56:24 +0200

    Lisätty logiikka raw-taulujen luomiseen

---
+++ dbt/macros/helpers/create_raw_tables.sql
@@ -0,0 +1,27 @@
+{% macro create_raw_tables() -%}
+
+{% set query %}
+    SELECT raw_taulu from {{ ref('raw_taulut') }}
+{% endset %}
+
+{% set tables = run_query(query) %}
+
+{%set tables_list = tables.columns[0].values()  %}
+
+{% if execute %}
+    {% for table in tables_list %}
+        {% set create_statement = 'create table if not exists raw.' + table +' (
+	"data" json NULL,
+	dw_metadata_source_timestamp_at timestamptz NULL,
+	dw_metadata_dbt_copied_at timestamptz NULL,
+	dw_metadata_filename varchar NULL,
+	dw_metadata_file_row_number int4 NULL
+    );
+    CREATE INDEX if not exists ix_'+ table + ' ON raw.' + table+ ' USING btree (dw_metadata_dbt_copied_at);'
+%}
+        {{ print ( create_statement )}}
+        {% do run_query(create_statement) %}
+        {% endfor %}
+
+{% endif %}
+{%- endmacro %}
