3dff3e4ea03ec2e4fa6b55777439fb6ed0523fcf
Mikko Siukola <mikko.siukola@cybercom.com>
2024-12-12 14:29:58 +0200

    OK-773 Disable bastion access logging

--- cdk/lib/bastion-stack.ts
+++ cdk/lib/bastion-stack.ts
@@ -2,7 +2,6 @@ import * as cdk from 'aws-cdk-lib';
 import * as asg from 'aws-cdk-lib/aws-autoscaling';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as elb from 'aws-cdk-lib/aws-elasticloadbalancingv2';
-import * as kms from 'aws-cdk-lib/aws-kms';
 import * as route53 from 'aws-cdk-lib/aws-route53';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
@@ -62,18 +61,18 @@ export class BastionStack extends cdk.Stack {
       }
     );
 
-    const nlbAccessLogsBucketName = `${config.environment}-bastion-nlb-access-logs`;
-    const nlbAccessLogsBucket = new s3.Bucket(this, nlbAccessLogsBucketName, {
-      objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      encryptionKey: new kms.Key(this, `${nlbAccessLogsBucketName}-s3BucketKMSKey`, {
-        enableKeyRotation: true,
-      }),
-      serverAccessLogsBucket: new s3.Bucket(
-        this,
-        `${nlbAccessLogsBucketName}-server-access-logs`
-      ),
-    });
+    // const nlbAccessLogsBucketName = `${config.environment}-bastion-nlb-access-logs`;
+    // const nlbAccessLogsBucket = new s3.Bucket(this, nlbAccessLogsBucketName, {
+    //   objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
+    //   blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    //   encryptionKey: new kms.Key(this, `${nlbAccessLogsBucketName}-s3BucketKMSKey`, {
+    //     enableKeyRotation: true,
+    //   }),
+    //   serverAccessLogsBucket: new s3.Bucket(
+    //     this,
+    //     `${nlbAccessLogsBucketName}-server-access-logs`
+    //   ),
+    // });
 
     const bastionNetworkLoadBalancer = new elb.NetworkLoadBalancer(
       this,
@@ -91,7 +90,7 @@ export class BastionStack extends cdk.Stack {
       }
     );
 
-    bastionNetworkLoadBalancer.logAccessLogs(nlbAccessLogsBucket);
+    //bastionNetworkLoadBalancer.logAccessLogs(nlbAccessLogsBucket);
 
     const bastionAutoScalingGroup = new asg.AutoScalingGroup(
       this,
@@ -214,6 +213,10 @@ export class BastionStack extends cdk.Stack {
       { id: 'AwsSolutions-L1', reason: 'TODO: Fix or add proper reason' },
       { id: 'AwsSolutions-AS3', reason: 'TODO: Fix or add proper reason' },
       { id: 'AwsSolutions-IAM4', reason: 'TODO: Fix or add proper reason' },
+      {
+        id: 'AwsSolutions-ELB2',
+        reason: 'TODO: Add access logs / are they even needed?',
+      },
     ]);
   }
 }
