afa1fcbeb26f11f1876bf4abba324ba72bba39b5
August Kilponen <august.kilponen@gmail.com>
2024-12-18 18:10:09 +0200

    Muutettu lukemaan konfiguraatio ympäristöparametreista

--- lampi-siirtaja-container/run.sh
+++ lampi-siirtaja-container/run.sh
@@ -7,7 +7,7 @@ echo "Running Lampi-siirtäjä..."
 start=$(date +%s)
 
 cd /root/lampi-siirtaja
-npm start "$POSTGRES_HOST" "$DB_USERNAME" "$DB_PASSWORD" "$LAMPI_S3_BUCKET"
+npm start
 
 echo "Ajon kesto `expr $(date +%s) - ${start}` s"
 
