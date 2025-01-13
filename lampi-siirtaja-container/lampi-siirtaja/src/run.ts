db9793e06bdf89448358ee9a59609ba0b5d41e11
August Kilponen <august.kilponen@gmail.com>
2025-01-13 18:07:21 +0200

    PassThrough stream käyttöön

--- lampi-siirtaja-container/lampi-siirtaja/src/run.ts
+++ lampi-siirtaja-container/lampi-siirtaja/src/run.ts
@@ -10,7 +10,7 @@ import {
 import { Upload } from '@aws-sdk/lib-storage';
 
 import MultiStream from 'multistream';
-import { Readable } from 'node:stream';
+import { PassThrough, Readable } from 'node:stream';
 
 const dbHost = process.env.POSTGRES_HOST;
 const dbUsername = process.env.DB_USERNAME;
@@ -77,40 +77,30 @@ const copyFileToLampi = async (sourceKey: string, numberOfFiles: number): Promis
 
   console.log(`${sourceKey} | tiedostojen määrä: ${numberOfFiles}`);
 
-  let bodyStream;
   let contentLength = 0;
-  if(numberOfFiles > 1) {
-    const streams = [];
-    for(let i = 1; i <= numberOfFiles; i++) {
-      const partSourceKey = i === 1 ? sourceKey : `${sourceKey}_part${i}`;
-      console.log(`${sourceKey} | partSourceKey: ${partSourceKey}`);
+  const streams = [];
+  for(let i = 1; i <= numberOfFiles; i++) {
+    const partSourceKey = i === 1 ? sourceKey : `${sourceKey}_part${i}`;
+    console.log(`${sourceKey} | partSourceKey: ${partSourceKey}`);
 
-      const getObjectCommandOutput: GetObjectCommandOutput = await ovaraS3Client.send(
-        new GetObjectCommand({
-          Bucket: ovaraLampiSiirtajaBucket,
-          Key: partSourceKey,
-        }),
-      );
-      streams.push(getObjectCommandOutput.Body);
-      contentLength = contentLength + getObjectCommandOutput.ContentLength;
-    }
-
-    bodyStream = new MultiStream(streams);
-  } else {
     const getObjectCommandOutput: GetObjectCommandOutput = await ovaraS3Client.send(
       new GetObjectCommand({
         Bucket: ovaraLampiSiirtajaBucket,
-        Key: sourceKey,
+        Key: partSourceKey,
       }),
     );
-    bodyStream = getObjectCommandOutput.Body;
-    contentLength = getObjectCommandOutput.ContentLength;
+    streams.push(getObjectCommandOutput.Body);
+    contentLength = contentLength + getObjectCommandOutput.ContentLength;
   }
+
+  const bodyStream = new MultiStream(streams);
+  const passThrough = new PassThrough();
+  bodyStream.pipe(passThrough);
 
   const target = {
     Bucket: lampiS3Bucket,
     Key: destinationKey,
-    Body: bodyStream,
+    Body: passThrough,
     ContentLength: contentLength,
     ContentType: 'text/csv'
   }
