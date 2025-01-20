5df617ecabac7f7b13511a339c236f3c9552685b
Matz Rasmus <matz.rasmus@knowit.fi>
2025-01-20 11:16:38 +0200

    valintarekisteri pub-skeemaan

---
+++ dbt/models/pub/pub_dim_valinnantulos.sql
@@ -0,0 +1,19 @@
+{{
+  config(
+    materialized = 'table',
+    indexes = [
+        {'columns': ['hakukohde_oid']}
+    ]
+    )
+}}
+
+{% set table = 'int_valintarekisteri_valinnantulos' %}
+
+with raw as
+(
+    select
+        {{ dbt_utils.star(from=ref(table),except=['valinnantulos_id']) }}
+    from {{ ref(table) }}
+)
+
+select * from raw
