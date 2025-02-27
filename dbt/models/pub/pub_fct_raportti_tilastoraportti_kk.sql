e5efa4bd864c4ee1419fb60dde4cad45d3d1c4ae
Matz Rasmus <matz.rasmus@knowit.fi>
2025-02-27 11:55:54 +0200

    OK-602 KK tilastoraportti

--- dbt/models/pub/pub_fct_raportti_tilastoraportti_kk.sql
+++ dbt/models/pub/pub_fct_raportti_tilastoraportti_kk.sql
@@ -1,37 +1,67 @@
 {{
-  config(
-    enabled=true
+    config(
+        indexes = [
+            {'columns': ['hakukohde_oid']}
+        ]
     )
 }}
 
-with hakukohde as (
-    select hako.*
-    from {{ ref('int_kouta_hakukohde') }} as hako
-    inner join {{ ref('int_kouta_haku') }} as haku on hako.haku_oid=haku.haku_oid and haku.haun_tyyppi = 'korkeakoulu'
+with hakutoive as (
+    select * from {{ ref('pub_dim_hakutoive') }}
 ),
 
-toteutus as (
-    select * from {{ ref('int_kouta_toteutus') }}
+henkilo as (
+    select * from {{ ref('pub_dim_henkilo') }}
 ),
 
-koulutus as (
-    select * from {{ ref('int_kouta_koulutus') }}
+valintarekisteri as (
+    select * from {{ ref('int_valintarekisteri') }}
 ),
 
-koulutustiedot as (
-    select * from {{ ref('int_koodisto_koulutus_alat_ja_asteet') }}
+hakemus as (
+    select * from {{ ref('int_hakemus_kk') }}
 ),
 
 final as (
     select
-        hako.hakukohde_oid,
-        koti.alempi_kk_aste::boolean,
-	      koti.ylempi_kk_aste::boolean,
-	      koti.okmohjauksenala
-    from hakukohde as hako
-    inner join toteutus as tote on hako.toteutus_oid = tote.toteutus_oid
-    inner join koulutus as koul on tote.koulutus_oid = koul.koulutus_oid
-    inner join koulutustiedot as koti on koul.koulutuksetkoodiuri ? koti.versioitu_koodiuri
+        hato.hakukohde_oid,
+        henk.aidinkieliluokka as aidinkieli,
+        coalesce(henk.kansalaisuusluokka, 3) as kansalaisuus,
+        henk.sukupuoli,
+        coalesce(hato.ensikertalainen, false) as ensikertalainen,
+        count(distinct hato.henkilo_oid) as hakijat,
+        sum(case when hakutoivenumero = '1' then 1 else 0 end) as ensisijaisia,
+        sum(1) as hyvaksytyt,
+        sum(
+            case
+                when vare.vastaanottotieto in ('VASTAANOTTANUT_SITOVASTI') then 1
+                else 0
+            end
+        ) as vastaanottaneet,
+        sum(1) as lasna,
+        sum(0) as poissa,
+        count(vare.ilmoittautumisen_tila) as ilm_yht,
+        sum(
+            case
+                when hake.maksuvelvollisuus = 'obligated' then 1
+                else 0
+            end
+        ) as maksuvelvollisia,
+        sum(1) as valinnan_aloituspaikat,
+        sum(1) as alpa,
+        sum(case when hakutoivenumero = '1' then 1 else 0 end) as toive_1,
+        sum(case when hakutoivenumero = '2' then 1 else 0 end) as toive_2,
+        sum(case when hakutoivenumero = '3' then 1 else 0 end) as toive_3,
+        sum(case when hakutoivenumero = '4' then 1 else 0 end) as toive_4,
+        sum(case when hakutoivenumero = '5' then 1 else 0 end) as toive_5,
+        sum(case when hakutoivenumero = '6' then 1 else 0 end) as toive_6,
+        sum(case when hakutoivenumero = '7' then 1 else 0 end) as toive_7
+    from hakutoive as hato
+    inner join henkilo as henk on hato.henkilo_hakemus_id = henk.henkilo_hakemus_id
+    left join valintarekisteri as vare on hato.hakukohde_henkilo_id = vare.hakukohde_henkilo_id
+    left join hakemus as hake on hato.hakutoive_id = hake.hakutoive_id
+    group by 1, 2, 3, 4, 5
+
 )
 
 select * from final
