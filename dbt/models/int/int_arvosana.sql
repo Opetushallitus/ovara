6f530f884c3fa3601f75d37c28af5aa6c6a7c884
Matz Rasmus <matz.rasmus@knowit.fi>
2025-03-04 08:27:31 +0200

    OK-602 YO-arvosanojen mappaus

---
+++ dbt/models/int/int_arvosana.sql
@@ -0,0 +1,31 @@
+{{
+  config(
+    materialized = 'table',
+    )
+}}
+
+with arvosana as (
+    select * from {{ ref('int_sure_arvosana') }}
+),
+
+suoritus as (
+    select * from {{ ref('int_sure_suoritus') }}
+),
+
+rivi as (
+    select
+        suoritus,
+        jsonb_object_agg (yo_aine, arvosana) as arvosanat
+    from arvosana
+    group by suoritus
+),
+
+final as (
+    select
+        suor.henkilo_oid,
+        rivi.arvosanat
+    from suoritus as suor
+    inner join rivi on suor.resourceid = rivi.suoritus
+)
+
+select * from final
