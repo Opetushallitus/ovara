059a32428fb8997b0958c30823b5f92fe7bedede
Matz Rasmus <matz.rasmus@knowit.fi>
2025-03-04 10:08:34 +0200

    OK-602 tilastoraporttien korjauksia

--- dbt/models/pub/pub_fct_raportti_tilastoraportti_kk.sql
+++ dbt/models/pub/pub_fct_raportti_tilastoraportti_kk.sql
@@ -10,6 +10,10 @@ with hakutoive as (
     select * from {{ ref('pub_dim_hakutoive') }}
 ),
 
+hakukohde as (
+    select * from {{ ref('pub_dim_hakukohde') }}
+),
+
 henkilo as (
     select * from {{ ref('pub_dim_henkilo') }}
 ),
@@ -31,6 +35,7 @@ final as (
         coalesce(hato.ensikertalainen, false) as ensikertalainen,
         count(distinct hato.henkilo_oid) as hakijat,
         sum(case when hakutoivenumero = '1' then 1 else 0 end) as ensisijaisia,
+        sum(case when ensikertalainen then 1 else 0 end) as ensikertalaisia,
         sum(1) as hyvaksytyt,
         sum(
             case
@@ -38,9 +43,38 @@ final as (
                 else 0
             end
         ) as vastaanottaneet,
-        sum(1) as lasna,
-        sum(0) as poissa,
-        count(vare.ilmoittautumisen_tila) as ilm_yht,
+        sum(case
+            when
+                hako.koulutuksen_alkamiskausi_koodiuri = 'kausi_s#1'
+                and upper(vare.ilmoittautumisen_tila) in
+                ('LASNA_SYKSY', 'POISSA_KEVAT', 'LASNA', 'LASNA_KOKO_LUKUVUOSI')
+                then 1
+            when
+                hako.koulutuksen_alkamiskausi_koodiuri = 'kausi_k#1'
+                and upper(vare.ilmoittautumisen_tila)
+                in ('LASNA_KEVAT', 'POISSA_SYKSY', 'LASNA', 'LASNA_KOKO_LUKUVUOSI')
+                then 1
+            when upper(vare.ilmoittautumisen_tila) in ('LASNA', 'LASNA_KOKO_LUKUVUOSI')
+                then 1
+            else 0
+        end) as lasna,
+        sum(case
+            when
+                hako.koulutuksen_alkamiskausi_koodiuri = 'kausi_s#1'
+                and upper(vare.ilmoittautumisen_tila) in ('POISSA_SYKSY', 'POISSA_KOKO_LUKUVUOSI') then 1
+            when
+                hako.koulutuksen_alkamiskausi_koodiuri = 'kausi_k#1'
+                and upper(vare.ilmoittautumisen_tila) in ('POISSA_KEVAT', 'POISSA_KOKO_LUKUVUOSI') then 1
+            when upper(vare.ilmoittautumisen_tila) in ('POISSA', 'POISSA_KOKO_LUKUVUOSI') then 1
+            else 0
+        end) as poissa,
+        sum(case
+            when vare.ilmoittautumisen_tila is not null
+            and vare.ilmoittautumisen_tila not in ('EI_TEHTY','EI_ILMOITTAUTUNUT')
+            then 1
+            else 0
+            end
+        ) as ilm_yht,
         sum(
             case
                 when hake.maksuvelvollisuus = 'obligated' then 1
@@ -55,9 +89,9 @@ final as (
         sum(case when hakutoivenumero = '4' then 1 else 0 end) as toive_4,
         sum(case when hakutoivenumero = '5' then 1 else 0 end) as toive_5,
         sum(case when hakutoivenumero = '6' then 1 else 0 end) as toive_6,
-        sum(case when hakutoivenumero = '7' then 1 else 0 end) as toive_7
     from hakutoive as hato
     inner join henkilo as henk on hato.henkilo_hakemus_id = henk.henkilo_hakemus_id
+    inner join hakukohde as hako on hato.hakukohde_oid = hako.hakukohde_oid
     left join valintarekisteri as vare on hato.hakukohde_henkilo_id = vare.hakukohde_henkilo_id
     left join hakemus as hake on hato.hakutoive_id = hake.hakutoive_id
     group by 1, 2, 3, 4, 5
