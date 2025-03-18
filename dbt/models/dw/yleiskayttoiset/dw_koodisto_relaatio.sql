84ff65b77e9f4013aa851a6628909fb3edcebbfa
Matz Rasmus <matz.rasmus@knowit.fi>
2025-03-18 14:28:13 +0200

    OK-602 Suorituskykyparannuksia yleiskäyttöiset

--- dbt/models/dw/yleiskayttoiset/dw_koodisto_relaatio.sql
+++ dbt/models/dw/yleiskayttoiset/dw_koodisto_relaatio.sql
@@ -1,4 +1,35 @@
-{%- set stage_model = ref('stg_koodisto_relaatio') -%}
-{%- set key_columns_list = ['koodirelaatio_id'] -%}
+{{
+  config(
+    materialized = 'incremental',
+    incremental_strategy = 'merge',
+    unique_key = 'koodirelaatio_id',
+    merge_exclude_columns = [
+            'dw_metadata_dbt_copied_at',
+            'dw_metadata_file_row_number',
+            'dw_metadata_filename',
+            'dw_metadata_source_timestamp_at',
+            'dw_metadata_dw_stored_at'
+            ],
+    indexes = [
+        {'columns': ['koodistouri']},
+        {'columns': ['muokattu']}
+    ]
+    )
+}}
 
-{{ generate_dw_model_muokattu(stage_model, key_columns_list) }}
+with raw as (
+    select * from {{ ref('stg_koodisto_relaatio') }}
+    {% if is_incremental() %}
+        where muokattu > (select coalesce(max(t.muokattu), date('1900-01-01')) from {{ this }} as t)
+    {% endif %}
+),
+
+final as (
+    select
+        *,
+        current_timestamp as dw_metadata_dw_stored_at
+    from raw
+)
+
+
+select * from final
