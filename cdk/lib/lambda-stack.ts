74a28e9d5822a471dd08d36b01adde36e974df99
Matz Rasmus <matz.rasmus@knowit.fi>
2025-05-15 10:21:45 +0300

    OK-979 Nostettu latauslambdan muisti maksimiin

--- cdk/lib/lambda-stack.ts
+++ cdk/lib/lambda-stack.ts
@@ -155,7 +155,7 @@ export class LambdaStack extends cdk.Stack {
         runtime: lambda.Runtime.NODEJS_22_X,
         architecture: lambda.Architecture.ARM_64,
         timeout: cdk.Duration.seconds(900),
-        memorySize: 4096,
+        memorySize: 10240,
         vpc: props.vpc,
         securityGroups: [lambdaSecurityGroup],
         role: executionRole,
