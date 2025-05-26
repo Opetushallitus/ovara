7210d6063e123aae510a8b65a6af092d89bd82e5
Matz Rasmus <matz.rasmus@knowit.fi>
2025-05-26 16:40:36 +0300

    OK-862 lisätty Koutan tauluja

---
+++ dbt/models/int1/int1_kouta/int_kouta_ammattinimike.sql
@@ -0,0 +1,1 @@
+select * from {{ ref('dw_kouta_ammattinimike') }}
--- dbt/models/int1/int_valintapiste_service_pistetieto.sql
+++ dbt/models/int1/int_valintapiste_service_pistetieto.sql
@@ -1,7 +1,5 @@
 with source as (
-    select
-        distinct on (valintakoe_hakemus_id)
-        *
+    select distinct on (valintakoe_hakemus_id) *
     from {{ ref('dw_valintapiste_service_pistetieto') }}
     order by valintakoe_hakemus_id asc, muokattu desc
 )
