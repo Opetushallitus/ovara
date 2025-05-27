5d4912d95097dc163e8ebba2e62ab32927805aef
Matz Rasmus <matz.rasmus@knowit.fi>
2025-05-27 16:12:30 +0300

    OK-979 Korjattu haun alkamiskausia

--- dbt/models/pub/pub_dim_haku.sql
+++ dbt/models/pub/pub_dim_haku.sql
@@ -12,14 +12,6 @@ with haku as (
     select * from {{ ref('int_haku') }}
 ),
 
-toteutus as (
-    select distinct
-        haku_oid,
-        koulutuksen_alkamiskausikoodi
-    from {{ ref('int_toteutus_koulutuksen_alkamiskausi') }}
-    where haku_oid is not null
-),
-
 hakutapakoodi as (
     select * from {{ ref('pub_dim_koodisto_hakutapa') }}
 ),
@@ -30,14 +22,6 @@ haunkohdejoukko as (
 
 haunkohdejoukontarkenne as (
     select * from {{ ref('pub_dim_koodisto_haunkohdejoukontarkenne') }}
-),
-
-toteutus_koulutuksen_alkamiskausi as (
-    select
-        haku_oid,
-        jsonb_agg(koulutuksen_alkamiskausikoodi) as koulutuksen_alkamiskausikoodi
-    from toteutus
-    group by 1
 ),
 
 step1 as (
@@ -56,13 +40,12 @@ step1 as (
         haku.kohdejoukontarkennekoodiuri,
         hatr.koodiarvo as kohdejoukontarkenne_koodi,
         hatr.koodinimi as kohdejoukontarkenne_nimi,
-        coalesce(haku.koulutuksen_alkamiskausikoodi, tote.koulutuksen_alkamiskausikoodi) as koulutuksen_alkamiskausi,
+        haku.koulutuksen_alkamiskausikoodi as koulutuksen_alkamiskausi,
         haku.haun_tyyppi
     from haku as haku
     inner join hakutapakoodi as hata on haku.hakutapakoodiuri = hata.versioitu_koodiuri
     inner join haunkohdejoukko as hajo on haku.kohdejoukkokoodiuri = hajo.versioitu_koodiuri
     left join haunkohdejoukontarkenne as hatr on haku.kohdejoukontarkennekoodiuri = hatr.versioitu_koodiuri
-    left join toteutus_koulutuksen_alkamiskausi as tote on haku.haku_oid = tote.haku_oid
 ),
 
 final as (
