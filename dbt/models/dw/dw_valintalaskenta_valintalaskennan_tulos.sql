e9e5ed46074c604ab69637b7b29fa33551d4e05c
Matz Rasmus <matz.rasmus@knowit.fi>
2025-06-04 09:19:29 +0300

    OK-979 korjaus dw_valintalaskenta_valintalaskennan_tulos

--- dbt/models/dw/dw_valintalaskenta_valintalaskennan_tulos.sql
+++ dbt/models/dw/dw_valintalaskenta_valintalaskennan_tulos.sql
@@ -9,23 +9,45 @@
 )
 }}
 
-with source as (
-    select distinct on (valinnanvaihe_id) *
+with tulos as not materialized (
+    select *
     from {{ ref('stg_valintalaskenta_valintalaskennan_tulos') }}
-    {% if is_incremental() -%}
-        {# Only rows which are newer than the rows in dw model table already #}
-        where (
-            dw_metadata_stg_stored_at > coalesce(
-                (select max(t.dw_metadata_stg_stored_at) from {{ this }} as t),
-                date('1900-01-01')
+    {% if target.name == 'prod' and is_incremental() %}
+    where dw_metadata_stg_stored_at > coalesce (
+	    (
+    		select  start_time from {{ source('ovara', 'completed_dbt_runs') }}
+	      	where raw_table = 'valintalaskenta_valintalaskennan_tulos'
+	    ),
+        '1900-01-01'
+    )
+    {% endif %}
+
+),
+
+int as (
+    select tls1.*
+    from tulos as tls1
+    left join tulos as tls2
+        on
+            tls1.valinnanvaihe_id = tls2.valinnanvaihe_id
+            and tls1.muokattu < tls2.muokattu
+    {% if is_incremental() %}
+        left join {{ this }} as tls3
+            on
+                tls1.valinnanvaihe_id = tls3.valinnanvaihe_id
+    {% endif %}
+    where
+        tls2.valinnanvaihe_id is null
+        {%- if is_incremental() %}
+            and (
+                tls1.muokattu > tls3.muokattu
+                or tls3.muokattu is null
             )
-            or dw_metadata_stg_stored_at is null
-        )
-    {%- endif %}
-    order by valinnanvaihe_id asc, muokattu desc
+        {%- endif %}
+
 )
 
 select
     *,
     current_timestamp as dw_metadata_dw_stored_at
-from source
+from int
