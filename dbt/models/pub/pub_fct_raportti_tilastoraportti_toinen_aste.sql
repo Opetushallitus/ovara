d6416d09cb08686791316a291a68fefbb1dca82a
Matz Rasmus <matz.rasmus@knowit.fi>
2025-06-09 13:10:47 +0300

    OK-979 Updating tags

--- dbt/models/pub/pub_fct_raportti_tilastoraportti_toinen_aste.sql
+++ dbt/models/pub/pub_fct_raportti_tilastoraportti_toinen_aste.sql
@@ -52,7 +52,12 @@ final as (
         count(distinct hato.henkilo_oid) as hakijat,
         sum(case when hato.hakutoivenumero = 1 then 1 else 0 end) as ensisijaisia,
         sum(case when hato.valintatieto = 'VARALLA' then 1 else 0 end) as varasija,
-        sum(case when hato.valintatieto = 'HYVAKSYTTY' then 1 else 0 end) as hyvaksytyt,
+        sum(case
+	        when hato.valintatieto = 'HYVAKSYTTY' and hato.vastaanottotieto != 'PERUUTETTU' then 1
+	        when hato.valintatieto = 'VARASIJALTA_HYVAKSYTTY' and hato.vastaanottotieto != 'PERUUTETTU' then 1
+	        when hato.valintatieto = 'PERUNUT' then 1
+	        else 0
+            end) as hyvaksytyt,
         sum(case when vare.vastaanottotieto in ('VASTAANOTTANUT_SITOVASTI') then 1 else 0 end) as vastaanottaneet,
         sum(case
             when
