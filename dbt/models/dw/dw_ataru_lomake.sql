c241c6859bf0eb69c1233d5166cf783dfc478532
Matz Rasmus <matz.rasmus@knowit.fi>
2025-06-30 10:25:05 +0300

    OK-1032 bugikorjaus ataru_lomake

--- dbt/models/dw/dw_ataru_lomake.sql
+++ dbt/models/dw/dw_ataru_lomake.sql
@@ -5,7 +5,7 @@
     incremental_strategy = 'merge',
     indexes = [
           {'columns': ['lomake_id_versioid_id','muokattu']},
-          {'columns': ['dw_metadata_dw_stored_at']},
+          {'columns': ['dw_metadata_stg_stored_at']},
           {'columns': ['kaksois_urheilija_tutkinto']}
       ],
     post_hook = "create index if not exists dw_lomake_id on {{ this }} ((content ->> 'id'))"
@@ -15,8 +15,8 @@
 with raw as (
     select distinct on (lomake_id_versioid_id, muokattu) * from {{ ref('stg_ataru_lomake') }}
     {% if is_incremental() %}
-        where dw_metadata_dbt_copied_at > coalesce(
-            (select max(t.dw_metadata_dw_stored_at) from {{ this }} as t),
+        where dw_metadata_stg_stored_at > coalesce(
+            (select max(t.dw_metadata_stg_stored_at) from {{ this }} as t),
             '1900-01-01'
         )
     {% endif %}
