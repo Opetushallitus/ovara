15ec7f31eb6160a31782b82597684e8847ac8bf3
August Kilponen <august.kilponen@gmail.com>
2025-11-07 15:26:16 +0200

    Muutettu DBT-kontin käynnistyskomentoa

--- ci-tools/run-testi-dbt-runner.sh
+++ ci-tools/run-testi-dbt-runner.sh
@@ -4,6 +4,20 @@ set -eo pipefail
 
 echo "Käynnistetään testin DBT Runner"
 
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --parameters*|-p*)
+      if [[ "$1" != *=* ]]; then shift; fi # Value is next arg if no `=`
+      parameters="${1#*=}"
+      ;;
+    *)
+      >&2 printf "Error: Invalid argument\n"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
 ecscluster=$(aws ecs list-clusters | jq -r '.clusterArns | .[0]' | cut -d "/" -f2)
 taskdefinition=$(aws ecs list-task-definitions --family-prefix testiEcsStacktestidbttaskScheduledTaskDef029C43B9 --sort DESC | jq -r '.taskDefinitionArns | .[0]' | cut -d "/" -f2)
 assignpublicip=$(aws events list-targets-by-rule --rule testi-scheduledFargateTaskRule | jq -r '.Targets | map(select(.Id == "Target0")) | .[0] | .EcsParameters.NetworkConfiguration.awsvpcConfiguration.AssignPublicIp')
@@ -14,7 +28,16 @@ awsvpcconfiguration=$(jq -c -n --argjson subnets "$subnets" \
                                --arg assignPublicIp "$assignpublicip" \
                                '$ARGS.named')
 networkconfiguration=$(jq -c -n --argjson awsvpcConfiguration "$awsvpcconfiguration" '$ARGS.named')
-command="aws ecs run-task --cluster $ecscluster --task-definition $taskdefinition --launch-type="FARGATE" --network-configuration '$networkconfiguration'"
-echo "$command"
 
-eval "$command"
+if [[ -z "$parameters" ]]; then
+  echo "Starting DBT without any extra paramaters"
+  command="aws ecs run-task --cluster $ecscluster --task-definition $taskdefinition --launch-type="FARGATE" --network-configuration '$networkconfiguration'"
+  echo "$command"
+  eval "$command"
+else
+  echo "Starting DBT with extra paramaters"
+  overrides="{ \"containerOverrides\": [ { \"name\": \"ScheduledContainer\"}, { \"command\": [ \"bash\", \"/root/run.sh\", \"--parameters=\\\"$parameters\\\"\" ] } ] }"
+  command="aws ecs run-task --cluster $ecscluster --task-definition $taskdefinition --launch-type='FARGATE' --network-configuration '$networkconfiguration' --overrides='$overrides'"
+  echo "$command"
+  eval "$command"
+fi
--- dbt-container/run.sh
+++ dbt-container/run.sh
@@ -10,6 +10,20 @@ echo "Running Ovara DBT script..."
 
 start=$(date +%s)
 
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --parameters*|-p*)
+      if [[ "$1" != *=* ]]; then shift; fi # Value is next arg if no `=`
+      parameters="${1#*=}"
+      ;;
+    *)
+      >&2 printf "Error: Invalid argument\n"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
 cd dbt
 . venv/bin/activate
 
@@ -26,7 +40,7 @@ aws dynamodb execute-statement --statement "UPDATE ecsProsessiOnKaynnissa SET on
 is_error="0"
 run_cleanup_and_generate_documentation="false"
 
-if [[ -z "$1" ]]; then
+if [[ -z "$parameters" ]]; then
   echo "Running DBT without any extra paramaters"
   dbt seed -s tag:seed --target=prod
   dbt run-operation create_raw_tables --target=prod
@@ -38,8 +52,10 @@ if [[ -z "$1" ]]; then
   fi
   echo "Finished running DBT"
 else
-  echo "Running DBT with extra paramaters: $1"
-  if dbt build --target=prod --exclude "resource_type:seed" "$1"; then
+  echo "Running DBT with extra paramaters: $parameters"
+  command="dbt build --target=prod --exclude \"resource_type:seed\" $parameters"
+  echo "$command"
+  if eval "$command"; then
   	is_error="0"
   else
     is_error="1"
