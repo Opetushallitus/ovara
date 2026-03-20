977187f456e179a6bd6ca657e68b1ec8cdb26902
Matz Rasmus <matz.rasmus@knowit.fi>
2026-03-20 09:30:33 +0200

    valinnantulos-taulusta disabloitu autovacuum

--- dbt/models/pub/pub_dim_valinnantulos.sql
+++ dbt/models/pub/pub_dim_valinnantulos.sql
@@ -3,7 +3,8 @@
     materialized = 'table',
     indexes = [
         {'columns': ['hakukohde_oid']}
-    ]
+    ],
+    post_hook = "alter table pub.pub_dim_valinnantulos set (autovacuum_enabled = false)"
     )
 }}
 
