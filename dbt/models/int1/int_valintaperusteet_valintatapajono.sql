3d7c703025f03f61eee5672aadf306953de1153d
Tomi Katajanheimo <tomi@katajanheimo.com>
2026-03-27 14:01:47 +0200

    OY-5546 Unnestattu valintatapajonot hakukohteen valinnanvaihesta

---
+++ dbt/models/int1/int_valintaperusteet_valintatapajono.sql
@@ -0,0 +1,50 @@
+{{
+  config(
+    materialized = 'table',
+    indexes = [
+        {'columns': ['jono_id']},
+        {'columns': ['hakukohde_oid']}
+    ]
+  )
+}}
+
+with hakukohde as (
+    select * from {{ ref('int_valintaperusteet_hakukohde') }}
+),
+
+valintatapajonoja as (
+    select
+        hakukohde_oid,
+        vaihe ->> 'id' as valinnanvaihe_id,
+        jsonb_array_elements(vaihe -> 'valintatapajono') as data
+    from hakukohde,
+        lateral jsonb_array_elements(valinnanvaiheet) as vaihe
+),
+
+rivit as (
+    select
+        data ->> 'oid' as jono_id,
+        valinnanvaihe_id,
+        hakukohde_oid,
+        data ->> 'nimi' as nimi,
+        data ->> 'kuvaus' as kuvaus,
+        (data ->> 'aloituspaikat')::int as aloituspaikat,
+        data ->> 'tyyppi' as tyyppi_uri,
+        (data ->> 'prioriteetti')::int as prioriteetti,
+        (data ->> 'siirretaanSijoitteluun')::boolean as siirretaan_sijoitteluun,
+        data ->> 'tasasijasaanto' as tasasijasaanto,
+        (data ->> 'eiLasketaPaivamaaranJalkeen')::timestamptz as ei_lasketa_paivamaaran_jalkeen,
+        (data ->> 'eiVarasijatayttoa')::boolean as ei_varasijatayttoa,
+        (data ->> 'merkitseMyohAuto')::boolean as merkitse_myoh_auto,
+        (data ->> 'poissaOlevaTaytto')::boolean as poissa_oleva_taytto,
+        (data ->> 'kaikkiEhdonTayttavatHyvaksytaan')::boolean as kaikki_ehdon_tayttavat_hyvaksytaan,
+        (data ->> 'kaytetaanValintalaskentaa')::boolean as kaytetaan_valintalaskentaa,
+        (data ->> 'valmisSijoiteltavaksi')::boolean as valmis_sijoiteltavaksi,
+        (data ->> 'valisijoittelu')::boolean as valisijoittelu,
+        (data ->> 'poistetaankoHylatyt')::boolean as poistetaanko_hylatyt,
+        data -> 'jarjestyskriteerit' as jarjestyskriteerit
+    from valintatapajonoja
+    where data ->> 'oid' is not null
+)
+
+select * from rivit
--- dbt/models/int1/int_valintarekisteri_valintatapajono.sql
+++ dbt/models/int1/int_valintarekisteri_valintatapajono.sql
@@ -11,26 +11,33 @@ with raw as (
     select * from {{ ref('dw_valintarekisteri_valintatapajono') }}
 ),
 
+vp as (
+    select jono_id, tyyppi_uri
+    from {{ ref('int_valintaperusteet_valintatapajono') }}
+),
+
 final as (
     select
-        oid as valintatapajono_oid,
-        nimi as valintatapajono_nimi,
-        hakukohde_oid,
-        alinhyvaksyttypistemaara,
-        alkuperaisetaloituspaikat,
-        aloituspaikat,
-        eivarasijatayttoa,
-        hakeneet,
-        kaikkiehdontayttavathyvaksytaan,
-        poissaolevataytto,
-        prioriteetti,
-        sijoiteltuilmanvarasijasaantojaniidenollessavoimassa,
-        tasasijasaanto,
-        valintaesityshyvaksytty,
-        varasijantayttopaivat,
-        varasijat,
-        varasijojakaytetaanasti
+        raw.oid as valintatapajono_oid,
+        raw.nimi as valintatapajono_nimi,
+        raw.hakukohde_oid,
+        raw.alinhyvaksyttypistemaara,
+        raw.alkuperaisetaloituspaikat,
+        raw.aloituspaikat,
+        raw.eivarasijatayttoa,
+        raw.hakeneet,
+        raw.kaikkiehdontayttavathyvaksytaan,
+        raw.poissaolevataytto,
+        raw.prioriteetti,
+        raw.sijoiteltuilmanvarasijasaantojaniidenollessavoimassa,
+        raw.tasasijasaanto,
+        raw.valintaesityshyvaksytty,
+        raw.varasijantayttopaivat,
+        raw.varasijat,
+        raw.varasijojakaytetaanasti,
+        vp.tyyppi_uri
     from raw
+    left join vp on raw.oid = vp.jono_id
 )
 
 select * from final
