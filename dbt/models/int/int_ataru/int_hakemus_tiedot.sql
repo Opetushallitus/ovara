3295f4719dbce0ca2e1e9d9c76259250d4a53517
Matz Rasmus <matz.rasmus@knowit.fi>
2026-04-10 10:04:04 +0300

    hakemusten tiedot incrementaliksi

--- dbt/models/int/int_ataru/int_hakemus_tiedot.sql
+++ dbt/models/int/int_ataru/int_hakemus_tiedot.sql
@@ -1,22 +1,34 @@
 {{
   config(
-    materialized = 'table',
+    materialized = 'incremental',
+    incremental_strategy= 'delete+insert',
+    unique_key = 'hakemus_oid',
     unlogged=true,
-    post_hook=[
-        "{{ disable_autovacuum_if_not_incremental() }}"
+    indexes = [
+        {'columns':['hakemus_oid']},
+        {'columns':['dw_metadata_dw_stored_at']}
     ]
     )
 }}
-with source as not materialized (
+
+with source as (
     select
         hakemus_oid,
-        tiedot
+        tiedot,
+        dw_metadata_dw_stored_at
     from {{ ref('int_ataru_hakemus') }}
+    {% if is_incremental() %}
+    where dw_metadata_dw_stored_at >= coalesce(
+            (select max(dw_metadata_dw_stored_at) from {{ this }}),
+            '1900-01-01'::timestamptz
+        )
+    {% endif %}
 )
 
 select
 	hakemus_oid,
 	e.key as kysymys,
-	e.value as vastaus
+	e.value as vastaus,
+    dw_metadata_dw_stored_at
 from source as hake
 cross join lateral jsonb_each(hake.tiedot) as e(key,value)
