4ea9872b97923b8921cf55ae046e4d85b826fc74
Matz Rasmus <matz.rasmus@knowit.fi>
2026-04-10 10:29:44 +0300

    gen_hakemus_tieto incrementaliksi

--- dbt/models/gen/gen_hakemus_tieto.sql
+++ dbt/models/gen/gen_hakemus_tieto.sql
@@ -1,16 +1,24 @@
 {{
-  config(
-    materialized = 'table',
+    config(
+    materialized = 'incremental',
+    incremental_strategy= 'delete+insert',
+    unique_key = 'hakemus_oid',
+    unlogged=true,
     indexes = [
-        {'columns' : ['hakemus_oid'] }
-    ],
-    post_hook=[
-        "{{ disable_autovacuum_if_not_incremental() }}"
+        {'columns':['hakemus_oid']},
+        {'columns':['dw_metadata_dw_stored_at']}
     ]
     )
 }}
+
 with source as not materialized (
     select * from {{ ref('int_hakemus_tiedot') }}
+    {% if is_incremental() %}
+        where dw_metadata_dw_stored_at >= coalesce(
+                (select max(dw_metadata_dw_stored_at) from {{ this }}),
+                '1900-01-01'::timestamptz
+        )
+    {% endif %}
 )
 
 select * from source
