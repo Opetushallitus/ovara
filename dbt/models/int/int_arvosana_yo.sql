691932a6853becffe8a7e02432710703aa755034
Matz Rasmus <matz.rasmus@knowit.fi>
2026-05-04 10:18:41 +0300

    Suorituskykyparannus yo_arvosana

--- dbt/models/int/int_arvosana_yo.sql
+++ dbt/models/int/int_arvosana_yo.sql
@@ -5,21 +5,20 @@
 }}
 
 with arvosana as (
-    select * from {{ ref('int_sure_arvosana') }}
+    select
+        suoritus,
+        yo_aine,
+        arvosana
+    from {{ ref('int_sure_arvosana') }}
     where asteikko = 'YO'
 ),
 
 suoritus as (
-    select * from {{ ref('int_sure_suoritus') }}
-    where not poistettu
-),
-
-rivi as (
     select
-        suoritus,
-        jsonb_object_agg(yo_aine, arvosana) as arvosanat
-    from arvosana
-    group by suoritus
+        resourceid,
+        henkilo_oid
+    from {{ ref('int_sure_suoritus') }}
+    where not poistettu
 ),
 
 final as (
@@ -27,7 +26,13 @@ final as (
         suor.henkilo_oid,
         rivi.arvosanat
     from suoritus as suor
-    inner join rivi on suor.resourceid = rivi.suoritus
+    inner join (
+        select
+            suoritus,
+            jsonb_object_agg(yo_aine, arvosana) as arvosanat
+        from arvosana
+        group by suoritus
+    ) as rivi on suor.resourceid = rivi.suoritus
 )
 
-select distinct * from final
+select * from final
