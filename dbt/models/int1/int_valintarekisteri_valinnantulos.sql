71cf67bc4e4d39221c727f8b431b4bb5167f0787
Matz Rasmus <matz.rasmus@knowit.fi>
2026-05-28 15:32:15 +0300

    Valinmtarekisterin bugi korjattu

--- dbt/models/int1/int_valintarekisteri_valinnantulos.sql
+++ dbt/models/int1/int_valintarekisteri_valinnantulos.sql
@@ -13,18 +13,18 @@
 }}
 
 with tulos as not materialized (
-    select *
+    select distinct on (valinnantulos_id) *
     from {{ ref('dw_valintarekisteri_valinnantulos') }}
     {% if target.name == 'prod' and is_incremental() %}
     where dw_metadata_dw_stored_at > coalesce (
 	    (
-    		select  start_time from {{ source('ovara', 'completed_dbt_runs') }}
+    		select start_time from {{ source('ovara', 'completed_dbt_runs') }}
 	      	where raw_table = 'valintarekisteri_valinnantulos'
 	    ),
         '1900-01-01'
     )
     {% endif %}
-
+    order by valinnantulos_id asc, muokattu desc
 ),
 
 int as (
