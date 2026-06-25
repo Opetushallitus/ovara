d2190cdf32840e3c43544390fa5a12836099de43
Matz Rasmus <matz.rasmus@knowit.fi>
2026-06-25 15:22:40 +0300

    suorituskykyparannus int_hakemus_tiedot

--- dbt/models/int/int_ataru/int_hakemus_tiedot.sql
+++ dbt/models/int/int_ataru/int_hakemus_tiedot.sql
@@ -8,11 +8,28 @@
         {'columns':['hakemus_oid']},
         {'columns':['dw_metadata_dw_stored_at']}
     ],
-    pre_hook = "set enable_seqscan = off;",
+    pre_hook = [
+        "set enable_seqscan = off;",
+        """{% if is_incremental() %}
+            with changed as materialized (
+                select distinct hakemus_oid
+                from {{ref('int_ataru_hakemus') }}
+                where dw_metadata_dw_stored_at >
+                (
+                    select max(dw_metadata_dw_stored_at)
+                    from {{ this }}
+                )
+            )
+            delete from {{ this }} t
+            using changed s
+            where t.hakemus_oid = s.hakemus_oid;
+            {% endif %}
+        """
+    ],
     post_hook = [
         "set enable_seqscan = on;",
         "create unique index if not exists int_hakemus_tiedot_hakemus_oid_kysymys_pk on {{ this }} (hakemus_oid, kysymys);"
-    ]
+        ]
     )
 }}
 with source as (
