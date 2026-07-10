747c6bf093d6928157e18997d8e37564dbea8e9f
Matz Rasmus <matz.rasmus@knowit.fi>
2026-07-10 13:19:19 +0300

    muokattu valintarekiserti_yos sarakkeet

--- dbt/models/int/int_valintarekisteri_yos.sql
+++ dbt/models/int/int_valintarekisteri_yos.sql
@@ -18,14 +18,7 @@ final as (
         yois.hakukohde_oid,
         yois.paatelty_aloituspvm,
         paat.obj ->> 'virtaOpiskeluOikeusId' as virta_opiskeluikeus_id,
-        paat.obj ->> 'organisaatioOid' as organisaatio_oid,
-        paat.obj -> 'supaNimi' ->> 'fi' as supa_nimi_fi,
-        paat.obj -> 'supaNimi' ->> 'sv' as supa_nimi_sv,
-        paat.obj -> 'supaNimi' ->> 'en' as supa_nimi_en,
-        paat.obj -> 'virtaNimi' ->> 'fi' as virta_nimi_fi,
-        paat.obj -> 'virtaNimi' ->> 'sv' as virta_nimi_sv,
-        paat.obj -> 'virtaNimi' ->> 'en' as virta_nimi_en,
-        muokattu
+        yois.muokattu
     from source as yois
     cross join lateral (select jsonb_array_elements(paatettavat_oikeudet)) as paat(obj)
 )
