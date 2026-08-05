e5cf483d525970f70d04723b244820133f564cb8
Matz Rasmus <matz.rasmus@knowit.fi>
2026-08-05 12:53:02 +0300

    typon korjaus

--- dbt/models/int1/supa/int_supa_opiskeluoikeus.sql
+++ dbt/models/int1/supa/int_supa_opiskeluoikeus.sql
@@ -7,7 +7,7 @@
         {'columns': ['henkilo_oid']},
         {'columns': ['dw_metadata_dw_stored_at']}
     ],
-    pre_hook = ["SET LOCAL enable_mergejoin = off;"]
+    pre_hook = ["SET LOCAL enable_mergejoin = off;"],
     post_hook = [
         "{{ create_pk('henkilo_oid') }}",
         "create index if not exists ix_supa_opiskeluoikeus_yo on {{ this }} (henkilo_oid) where jsonb_array_length(data -> 'yoOpiskeluoikeudet') > 0;",
