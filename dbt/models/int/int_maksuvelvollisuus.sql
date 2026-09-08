798e753721877c191431e829c5113dd0b615fede
Matz Rasmus <matz.rasmus@knowit.fi>
2026-09-08 13:52:36 +0300

    Muutettu kk-haun päättely

--- dbt/models/int/int_maksuvelvollisuus.sql
+++ dbt/models/int/int_maksuvelvollisuus.sql
@@ -8,29 +8,33 @@
     )
 }}
 
-with hakemus as ( --noqa: PRS
-    select * from {{ ref('int_ataru_hakemus') }}
-    where kasittelymerkinnat @? '$[*] ? (@.requirement == "payment-obligation")'
+with kk_haku as (
+    select haku_oid from {{ ref('int_kouta_haku') }}
+    where haun_tyyppi = 'korkeakoulu'
+),
+
+
+hakemus as ( --noqa: PRS
+    select * from {{ ref('int_ataru_hakemus') }} as hake
+    where exists (select 1 from kk_haku as haku where hake.haku_oid=haku.haku_oid )
 ),
 
 rows as (
-    select hakemus_oid,
-        (
-            jsonb_path_query(
-            kasittelymerkinnat,
+    select
+        hake.hakemus_oid,
+        merk.merkinta ->> 'hakukohde' as hakukohde_oid,
+        merk.merkinta ->> 'state' as maksuvelvollisuus
+    from hakemus as hake
+    left join lateral (
+        select jsonb_path_query(
+            hake.kasittelymerkinnat,
             '$[*] ? (@.requirement == "payment-obligation")'
-            ) ->> 'hakukohde'
-        ) as hakukohde_oid,
-        (
-            jsonb_path_query(
-            kasittelymerkinnat,
-            '$[*] ? (@.requirement == "payment-obligation")'
-            ) ->> 'state'
-        ) as maksuvelvollisuus
-    from hakemus
+        ) as merkinta
+    ) as merk on true
 )
 
 select
     {{ hakutoive_id() }},
     maksuvelvollisuus
 from rows
+where hakukohde_oid is not null
